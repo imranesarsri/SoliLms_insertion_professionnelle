77a95621cae5894651e5ca0427a4a6dc94202811
imrane sarsri <imransarsi.ii@gmail.com>
2024-01-25 17:37:25 +0100

    merge develop

---
+++ Conseption/2.starter_maquettage/storage/framework/views/58bcc561c6f42464daeecd9b79f4007d.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Server Error')); ?>
+<?php $__env->startSection('code', '500'); ?>
+<?php $__env->startSection('message', __('Server Error')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH S:\github\SoliLms_insertion_professionnelle\Conseption\2.starter_maquettage\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/500.blade.php ENDPATH**/ ?>
