c25cd41f5c791e228b09255ab2d285bc7575e2ec
imrane sarsri <imransarsi.ii@gmail.com>
2024-01-28 02:44:49 +0100

    save

--- Conseption/3.maquettage/resources/views/Layouts/footer.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- footer --}}
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-    </div>
-    <strong>Copyright © 2023-2024 <a href="https://adminlte.io">SoliLms</a>.</strong> Tous droits réservés.
-</footer>
